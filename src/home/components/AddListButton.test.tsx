8eea026fcf077a179c97b47afbbf10da49107ef2
bthreader <threaderb@hotmail.co.uk>
2023-02-26 18:18:36 +0000

    organize files, add a commented out test to test closing the dialog

--- src/home/components/AddListButton.test.tsx
+++ src/home/components/AddListButton.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
 import AddListButton from "./AddListButton";
-import MockMsalContext from '../../core/MockMsalContext';
+import MockMsalContext from '../../core/testing/MockMsalContext';
 
 jest.mock('@azure/msal-react', () => ({
     useMsal: () => MockMsalContext,
@@ -12,12 +12,13 @@ jest.mock('axios');
 
 describe('AddListButton', () => {
 
-    it('loads and displays speed dial icon', async () => {
+    it('loads and displays speed dial icon but not dialog', async () => {
         const mockOnSubmit = jest.fn(); 
 
         render(<AddListButton triggerListsChanged={mockOnSubmit}/>);
 
         expect(screen.getByLabelText('Add list button')).toBeVisible();
+        expect(screen.queryByLabelText('Add list dialog')).toBeNull();
     })
 
     it('opens task dialog when button is clicked', async () => {
@@ -27,8 +28,20 @@ describe('AddListButton', () => {
         render(<AddListButton triggerListsChanged={mockOnSubmit}/>);
 
         await user.click(screen.getByLabelText('Add list button'));
+
         expect(screen.getByLabelText('Add list dialog')).toBeVisible();
     })
+
+    // it('closes task dialog when non-dialog area is clicked', async () => {
+    //     const user = userEvent.setup()
+    //     const mockOnSubmit = jest.fn(); 
+
+    //     render(<AddListButton triggerListsChanged={mockOnSubmit}/>)
+
+    //     await user.click(screen.getByLabelText('Add list button'));
+
+    //     expect(screen.queryByLabelText('Add list dialog')).toBeNull();
+    // })
 
     it('calls the on submit function when add button is clicked on dialog',
         async () => {
@@ -40,6 +53,7 @@ describe('AddListButton', () => {
             await user.click(screen.getByLabelText('Add list button'));
             await user.type(screen.getByLabelText('List name'), 'My List');
             await user.click(screen.getByLabelText('Submit add list'));
+
             expect(mockOnSubmit).toBeCalled();
         }
     )
